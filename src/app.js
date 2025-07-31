c00880c54a2aeee4f87c3341fcf57b57e2b4633a
VictorPko73 <152915385+VictorPko73@users.noreply.github.com>
2025-07-31 09:05:58 +0000

    logica y carta creada

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+   function cartaAleatoria() {
+    const valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+    const palos = ["♠️", "♥️", "♦️", "♣️"];
+
+    const valor = valores[Math.floor(Math.random() * valores.length)];
+    const palo = palos[Math.floor(Math.random() * palos.length)];
+
+    return { valor, palo };
+  }
+  const carta = cartaAleatoria();
+
+  document.getElementById("numberRandom").innerHTML = carta.valor;
+  document.getElementById("randomSuit").innerHTML = carta.palo;
+  document.getElementById("randomSuit2").innerHTML = carta.palo;
+  
 };
